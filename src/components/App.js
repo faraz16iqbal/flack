d647701e1aa083e05ae4ec9524e9103c80202d45
Faraz Iqbal <faraziqbal@hotmail.co.uk>
2020-10-15 20:03:03 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,12 @@ import SidePanel from "./SidePanel/SidePanel";
 import Messages from "./Messages/Messages";
 import MetaPanel from "./MetaPanel/MetaPanel";
 
-const App = ({ currentUser }) => (
+const App = ({ currentUser, currentChannel }) => (
   <Grid columns="equal" className="app" style={{ backgroundColor: "#eee" }}>
     <ColorPanel />
     <SidePanel currentUser={currentUser} />
     <Grid.Column style={{ marginLeft: 320 }}>
-      <Messages />
+      <Messages currentChannel={currentChannel} />
     </Grid.Column>
     <Grid.Column width={4}>
       <MetaPanel />
@@ -23,5 +23,6 @@ const App = ({ currentUser }) => (
 
 const mapStateToProps = (state) => ({
   currentUser: state.user.currentUser,
+  currentChannel: state.channel.currentChannel,
 });
 export default connect(mapStateToProps)(App);
--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -2,12 +2,30 @@ import React, { Component } from "react";
 import { Segment, Button, Input } from "semantic-ui-react";
 
 export default class MessageForm extends Component {
+  state = {
+    message: "",
+    loading: false,
+  };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  sendMessage = () => {
+    const { messagesRef } = this.props;
+    const { message } = this.state;
+    if (message) {
+      this.setState({ loading: true });
+    }
+  };
+
   render() {
     return (
       <Segment className="message__form">
         <Input
           fluid
           name="message"
+          onChange={this.handleChange}
           style={{ marginBottom: "0.7em" }}
           label={<Button icon="add" />}
           labelPosition="left"
@@ -15,6 +33,7 @@ export default class MessageForm extends Component {
         />
         <Button.Group icon widths="2">
           <Button
+            onClick={this.sendMessage}
             color="orange"
             content="Add Reply"
             labelPosition="left"
